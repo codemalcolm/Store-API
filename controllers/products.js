66a4cb386d8c7b1893abad8b3bf098071e8bfee9
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-03-08 10:23:28 +0100

    NumericFilters added

--- controllers/products.js
+++ controllers/products.js
@@ -2,13 +2,13 @@ const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
 	const search = "Sectional";
-	const products = await Product.find({}).limit(10);
+	const products = await Product.find({price:{$lt:30}}).limit(10);
 	res.status(200).json({nbHits: products.length, products: products});
 };
 
 const getAllProducts = async (req, res) => {
 	// destructuring request query
-	const { featured, company, name, price, rating, sort, fields } = req.query;
+	const { featured, company, name, price, rating, sort, fields, numericFilters } = req.query;
 	const queryObject = {};
 
 	// checking if featured exists
@@ -40,9 +40,34 @@ const getAllProducts = async (req, res) => {
 		// if price exists it's value is assigned to the queryObject
 		queryObject.price = price;
 	}
+	
+	// checking if numericFilters exists
+	if (numericFilters) {
+		const numericFiltersList = numericFilters.split(",").join(" ");
+		const operatorMap = {
+			">" : "$gt",
+			"<" : "$lt",
+			"=" : "$eq",
+			">=" : "$gte",
+			"<=" : "$lte"
+		}
+		const regEx = /\b(=|>|<|<=|>=)\b/g
 
-    
-    
+		let filters = numericFilters.replace(regEx, (match)=> `-${operatorMap[match]}-`)
+
+		const options = ['price','rating'];
+
+		filters = filters.split(',').forEach((item)=> {
+			const [field,operator,value] = item.split('-');
+			if(options.includes(field)){
+				queryObject[field] = { [operator]: Number(value) }
+			}
+		})
+
+		// if price exists it's value is assigned to the queryObject
+		console.log(filters);
+	}
+
 	// find all products with queryObject
 	let result = Product.find(queryObject);
 
@@ -62,6 +87,7 @@ const getAllProducts = async (req, res) => {
         result = result.select({});
 	}
 
+	// page logic
     const page = Number(req.query.page) || 1;
     const limit = Number(req.query.limit) || 10;
     const skip = (page-1) * limit;
